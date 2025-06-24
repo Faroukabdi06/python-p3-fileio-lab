932beb94fa3070fb9a5619de5a270b90748ff83a
Farouk Abdi <abdifarouk58@gmail.com>
2025-06-24 14:15:39 +0300

    completed test

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
-def write_file(file_name, file_content):
-    pass
+from pathlib import Path
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    file_name = Path(file_name)
+    if file_name.suffix != ".txt":
+        file_name = file_name.with_suffix(".txt")
+    with open(file_name, "w") as f:
+        f.write(file_content)
+
+def append_file(file_name, file_content):
+    file_name = Path(file_name)
+    if file_name.suffix != ".txt":
+        file_name = file_name.with_suffix(".txt")
+    with open(file_name, "a") as f:
+        f.write(file_content)
 
 def read_file(file_name):
-    pass
+    file_name = Path(file_name)
+    if file_name.suffix != ".txt":
+        file_name = file_name.with_suffix(".txt")
+    with open(file_name, "r") as f:
+        return f.read()
